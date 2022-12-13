188a036fa72f30d7839ada05bff7a70f31fc2ca9
China <ibuakaezechina@gmail.com>
2022-12-13 06:39:14 -0600

    drop tables if they exist

--- create_tables.py
+++ create_tables.py
@@ -1,10 +1,13 @@
 import psycopg2
 
+def drop_tables(cur):
+    cur.execute("DROP TABLE IF EXISTS songs, users, artists, time, songplays")
+
 def create_songs_table(cur):
     #Create dimension table for songs
     try:
         cur.execute("CREATE TABLE IF NOT EXISTS songs(song_id VARCHAR(255) PRIMARY KEY, title VARCHAR(255), artist_id VARCHAR(255), \
-            year INT, duration NUMERIC)")
+            year INT, duration FLOAT)")
     except psycopg2.Error as e:
         print(e)
 
@@ -20,22 +23,22 @@ def create_artists_table(cur):
     #Create dimension table for artists
     try:
         cur.execute("CREATE TABLE IF NOT EXISTS artists(artist_id VARCHAR(255) PRIMARY KEY, name VARCHAR(255), location VARCHAR(255), \
-        latitude NUMERIC, longitude NUMERIC)")
+        latitude FLOAT, longitude FLOAT)")
     except psycopg2.Error as e:
         print(e)
 
 def create_time_table(cur):
     #Create dimension table for time
     try:
-        cur.execute("CREATE TABLE IF NOT EXISTS time(time_id INT PRIMARY KEY, start_time TIME, hour TIME, day DATE, week TEXT, \
-            month DATE, year DATE, weekday VARCHAR(50))")
+        cur.execute("CREATE TABLE IF NOT EXISTS time(start_time VARCHAR(255) PRIMARY KEY, hour INT, day INT, week INT, \
+            month INT, year INT, weekday VARCHAR(50))")
     except psycopg2.Error as e:
         print(e)
 
 def create_songplays(cur):
     #Create facts table for songplays
     try:
-        cur.execute("CREATE TABLE IF NOT EXISTS songplays(songplay_id VARCHAR(255) PRIMARY KEY, start_time TIME, user_id INT, level_song TEXT, \
+        cur.execute("CREATE TABLE IF NOT EXISTS songplays(songplay_id SERIAL PRIMARY KEY, start_time TIME, user_id INT, level TEXT, song_id VARCHAR(255), \
             artist_id VARCHAR(255), session_id INT, location VARCHAR(255), user_agent VARCHAR(255))")
     except psycopg2.Error as e:
         print(e)
